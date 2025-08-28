c0ba003d81a0acfd2c847b9c90c2ce860b2da349
lpreaux <contact@lucaspreaux.fr>
2025-08-28 16:39:03 +0200

    add langage detection to middleware

---
+++ src/middlewares.ts
@@ -0,0 +1,43 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { type NextRequest, NextResponse } from "next/server";
+
+const locales = ["en-US", "fr-FR"];
+const defaultLocale = "en-US";
+
+function getLocale(request: NextRequest) {
+  // Convertir les headers Next.js en objet plain
+  const headers: Record<string, string> = {};
+  request.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
